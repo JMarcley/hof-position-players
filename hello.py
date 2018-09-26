72c69d1321e0eeb57d6aca541bdcbb20d71b5587
JMarcley <joshua.marcley@gmail.com>
2018-09-26 12:32:18 -0700

    update urls

--- hello.py
+++ hello.py
@@ -3,18 +3,18 @@ from flask import render_template
 
 app = Flask(__name__)
 
-@app.route("/hello")
-def hello():
-    return "Hello World!"
+# @app.route("/hello")
+# def hello():
+#     return "Hello World!"
+#
+# @app.route("/one")
+# def one():
+#     return render_template("one.html")
+#
+# @app.route("/two")
+# def two():
+#     return render_template("sample_two.html")
 
-@app.route("/one")
-def one():
-    return render_template("one.html")
-
-@app.route("/two")
-def two():
-    return render_template("sample_two.html")
-
-@app.route("/three")
+@app.route("/")
 def three():
     return render_template("sample_three.html")
